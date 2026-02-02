5d909ee2a0ddb86237b1c326c93d9d05855152b3
Eli Fried <eli.fried2004@gmail.com>
2026-02-02 20:27:46 +0100

    Update script_part2a.py
    kommentarer i koden

--- roadster/script_part2a.py
+++ roadster/script_part2a.py
@@ -11,16 +11,17 @@ y=distance_elsa[-1]
 #t_anna=rsd.time_to_destination(x,'speed_anna.npz',n)
 #t_elsa=rsd.time_to_destination(y,'speed_elsa.npz',n)
 
-
-
-I_refa = rsd.time_to_destination(x,'speed_anna.npz',100000)
-
-
-I_refe = rsd.time_to_destination(y,'speed_elsa.npz',100000)
-
 print(f'Tid för Elsa att köra sin rutt: {60* I_refe} minuter, tid för Anna att kköra sin rutt: {I_refa * 60} minuter')
 
 
+# kollar hur många n som behövs för att kunna få rätt minut.
+
+
+#Skapar referenser som integralerna med kortare steglängd kommer jämföas med för anna och elsa
+
+I_refa = rsd.time_to_destination(x,'speed_anna.npz',100000)
+
+I_refe = rsd.time_to_destination(y,'speed_elsa.npz',100000)
 
 n= 2
 värden_a= np.array([])
@@ -28,6 +29,7 @@ steg_a = np.array([])
 värden_e = np.array([])
 steg_e = np.array([])
 
+#dubblar steglängden i slutet av varje iteration för att förhoppnigsvis komma uner gränsen för på minuten
 
 for i in range(11):
 
@@ -44,17 +46,14 @@ for i in range(11):
     n=2*n
 
 
-
-
-
-
 plt.figure()
 plt.loglog(steg_a,värden_a, label = 'Anna')
 plt.loglog(steg_e, värden_e, label = 'Elsa')
-plt.axhline(y=(1/120), color='r', linestyle='--', label='Gräns: 30s')
+plt.axhline(y=(1/120), color='r', linestyle='--', label='Gräns: 30s') # plottar även med streckad linje där 30 sekunder är för att hamnar v under den är vi relativt bra på minuten. 
 plt.xlabel('n')
 plt.ylabel('tidsskillnad')
 plt.grid(True)
 plt.show()
+
 
 
